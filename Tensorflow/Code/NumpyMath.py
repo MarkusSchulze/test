3ed78bf4a4361207ac37c9049cd7f1f4861a4bb8
schulze <schulze@hermes>
2018-01-08 17:59:38 +0100

    starter

---
+++ Tensorflow/Code/NumpyMath.py
@@ -0,0 +1,18 @@
+'''
+Created on 08.01.2018
+
+@author: schulze
+'''
+import numpy as np
+
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y=5
+    return y+a
+
+print(add(1, 2))
+print(addFixedValue(1))
+print("test")
+
